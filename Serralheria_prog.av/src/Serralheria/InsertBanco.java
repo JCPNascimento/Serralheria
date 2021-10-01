93e29ebe2e65ba1c94021c819872c9733d8a090e
ThayaneLopes <tl.1004@outlook.com>
2021-09-30 21:34:48 -0300

    Metodos insert banco

--- Serralheria_prog.av/src/Serralheria/InsertBanco.java
+++ Serralheria_prog.av/src/Serralheria/InsertBanco.java
@@ -36,5 +36,89 @@ public class InsertBanco {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public void salvarEndereco(Endereco endereco) {
+		try {
+
+			String sql = "INSERT INTO ENDERECO (RUA,PONTO_REFERENCIA,ESTADO,CIDADE,BAIRRO,NUMERO,CEP) VALUES (?,?,?,?,?,?,?)";
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, endereco.getRua());
+			stmt.setString(2, endereco.getPontRefe());
+			stmt.setString(3, endereco.getEstado());
+			stmt.setString(4, endereco.getCidade());
+			stmt.setString(5, endereco.getBairro());
+			stmt.setString(6, endereco.getNumero());
+			stmt.setString(7, endereco.getCep());
+			stmt.execute();
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void salvarCliente(Cliente cliente) {
+		try {
+
+			String sql = "INSERT INTO CLIENTE (CPF,TELEFONE,ID_ENDERECO) VALUES (?,?,?)";
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, cliente.getCpf());
+			stmt.setString(2, cliente.getTelefone());
+			stmt.setInt(3, cliente.getEndereco().getId());
+			stmt.execute();
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void salvarTecnico(Tecnico tecnico) {
+		try {
+
+			String sql = "INSERT INTO TECNICO (NOME,TELEFONE,CPF,CODIGO_ACESSO) VALUES (?,?,?,?)";
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, tecnico.getNome());
+			stmt.setString(2, tecnico.getTelefone());
+			stmt.setString(3, tecnico.getCpf());
+			stmt.setDouble(4, tecnico.getCodcesso());
+			stmt.execute();
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public void salvarOrcamento(Orcamento orcamento) {
+		try {
+
+			String sql = "INSERT INTO ORCAMENTO (ALUMINIO,ALTURA,LARGURA,METROS,TIPO_MATERIAL,TOTAL) VALUES (?,?,?,?,?,?)";
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setDouble(1, orcamento.get$aluminio());
+			stmt.setDouble(2, orcamento.getAltura());
+			stmt.setDouble(3, orcamento.getLargura());
+			stmt.setDouble(4, orcamento.getMetrosqd());
+			stmt.setInt(5, orcamento.getTipmaterial());
+			stmt.setDouble(6, orcamento.getTotal());
+			stmt.execute();
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public void salvarPagamento(FormaPagamento pagamento) {
+		try {
+
+			String sql = "INSERT INTO PAGAMENTO (TIPO_PAGAMENTO,TOTAL_PARCELAS,VALOR_TOTAL,VALOR_JUROS) VALUES (?,?,?,?)";
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setInt(1, pagamento.getTipopagamento());
+			stmt.setInt(2, pagamento.getVezes());
+			stmt.setDouble(3, pagamento.getValortot());
+			stmt.setDouble(4, pagamento.getCredito(0, 0));
+			stmt.execute();
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 
 }
